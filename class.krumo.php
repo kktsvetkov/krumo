c50701e3a9c83b2ee972a58696f09b9df3316f58
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2017-11-20 14:49:18 +0200

    adding getallheaders() fallback
    * on nginx/php-fpm I got “Fatal error: Call to undefined function
    getallheaders() in …/krumo/class.krumo.php on line 229”
    * added home-made getallheaders() from
    https://stackoverflow.com/a/17590565

--- class.krumo.php
+++ class.krumo.php
@@ -219,6 +219,29 @@ This is a list of all currently loaded PHP extensions.
 			return false;
 		}
 
+		if (!function_exists('getallheaders'))
+		{
+			function getallheaders()
+			{
+				$headers = array ();
+				foreach ($_SERVER as $name => $value)
+				{
+					if (substr($name, 0, 5) == 'HTTP_')
+					{
+						$key = str_replace(
+							' ',
+							'-',
+							ucwords(strtolower(
+								str_replace('_', ' ', substr($name, 5))
+								))
+							);
+						$headers[$key] = $value;
+					}
+				}
+				return $headers;
+			}
+		}
+
 		// render it
 		//
 		?>
